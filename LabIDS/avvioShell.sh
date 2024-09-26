616c198b45d12d4353a9582ec483402d8e50c720
Luigi810 <55115564+Luigi810@users.noreply.github.com>
2024-09-26 19:50:57 +0200

    Update avvioShell.sh

--- LabIDS/avvioShell.sh
+++ LabIDS/avvioShell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Ottieni gli ID dei container in esecuzione
+# Valori ID dei container in esecuzione
 containers=$(docker ps -q)
 
-# Apri una nuova finestra per ogni container
+# Apre una nuova finestra per ogni container
 for container in $containers; do
     gnome-terminal -- bash -c  "docker exec -it $container bash" &
 done
