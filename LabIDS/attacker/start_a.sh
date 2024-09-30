9b9f7e6e2a2e7e048f77b03196353e5a6efc5a4c
Luigi810 <55115564+Luigi810@users.noreply.github.com>
2024-09-30 10:07:34 +0200

    Update start_a.sh
    Non serve che l'attacker resti attivo

--- LabIDS/attacker/start_a.sh
+++ LabIDS/attacker/start_a.sh
@@ -4,4 +4,4 @@
 ip route add 172.18.0.0/16 via 172.19.0.2
 
 
-tail -f /dev/null    #per tenere il container aperto, altrimenti si chiuderebbe da  solo
+#tail -f /dev/null    #per tenere il container aperto, altrimenti si chiuderebbe da  solo
